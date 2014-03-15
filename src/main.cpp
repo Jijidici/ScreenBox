127fbf8219d78afb400f9b842d0ee432a20effdf
Jijidici <jdefaye.jijidici@gmail.com>
2014-03-15 19:45:17 +0100

    Create the VS2012 project

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/* *****************************************************
+ *	main.cpp - Jijidici - 15/03/2014
+ * ***************************************************** */
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char** argv) {
+	std::cout << "> PROGRAM LAUNCH" << std::endl;
+
+	std::cout << "> PROGRAM EXIT" << std::endl;
+	return EXIT_SUCCESS;
+}
