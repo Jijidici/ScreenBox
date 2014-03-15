b56c5ce49b82c9f813e831cbb20c1dc46e1f8e4f
Jijidici <jdefaye.jijidici@gmail.com>
2014-03-15 22:01:15 +0100

    Set a default render loop

--- src/ScreenBox.cpp
+++ src/ScreenBox.cpp
@@ -7,15 +7,15 @@
 #define GLFW_DLL
 
 #include <iostream>
-#include "GL/glew.h"
-#include "glfw3.h"
+#include <string>
+#include <sstream>
 
 void ScreenBox::init() {
 	std::cout << "> INIT SCREENBOX" <<std::endl;
 
+	 _pWindow = NULL;
 	_iW = 800;
 	_iH = 600;
-	_bLoop = true;
 
 	 // Initialise GLFW
     if(!glfwInit()) {
@@ -24,17 +24,61 @@ void ScreenBox::init() {
     }
 
 	// Open a window
-	GLFWwindow* window = glfwCreateWindow(_iW, _iH, "ScreenBox", NULL, NULL);
+	_pWindow = glfwCreateWindow(_iW, _iH, "ScreenBox", NULL, NULL);
+	if (!_pWindow) {
+        std::cout << "Failed to create a window" <<std::endl;
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+	// Handle key events
+	glfwSetKeyCallback(_pWindow, ScreenBox::onKey);
+
+	// Use OpenGL context
+	glfwMakeContextCurrent(_pWindow);
+
+	// OpenGL configs
+	glClearColor(0.2f, 0.2f, 0.2f, 1.f);
 }
 
 void ScreenBox::launch() {
 	std::cout << "> LAUNCH SCREENBOX" <<std::endl;
 
-	while(_bLoop) {
+	while(!glfwWindowShouldClose(_pWindow)) {
+		_dTime = glfwGetTime();
 
+		/* *************************************************** *
+		 * ********* RENDER ZONE	
+		 * *************************************************** */
+		glClear(GL_COLOR_BUFFER_BIT);
+		glViewport(0, 0, _iW, _iH);
+
+		glfwSwapBuffers(_pWindow);
+
+		/* *************************************************** *
+		 * ********* EVENT ZONE	
+		 * *************************************************** */
+		glfwPollEvents();
+
+		//manage fps
+		double dNewTime = glfwGetTime();
+		_fFPS = 1.f/static_cast<float>(dNewTime-_dTime);
+
+		std::stringstream fpsStream;
+		fpsStream << "Screen Box - " << _fFPS << "FPS";
+		glfwSetWindowTitle(_pWindow, fpsStream.str().c_str());
 	}
 }
 
 void ScreenBox::destroy() {
 	std::cout << "> DESTROY SCREENBOX" <<std::endl;
+	glfwDestroyWindow(_pWindow);
+	glfwTerminate();
+}
+
+void ScreenBox::onKey(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	//exit the app
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
 }
--- src/ScreenBox.h
+++ src/ScreenBox.h
@@ -5,16 +5,24 @@
 #ifndef __SCREEN_BOX_H__
 #define __SCREEN_BOX_H__
 
+#include "GL/glew.h"
+#include "glfw3.h"
+
 class ScreenBox {
 public:
 	void init();
 	void launch();
 	void destroy();
 
+
 private:
+	static void onKey(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+	GLFWwindow* _pWindow;
 	int _iW;
 	int _iH;
-	bool _bLoop;
+	double _dTime;
+	float _fFPS;
 };
 
 #endif //__SCREEN_BOX_H__
